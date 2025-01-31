5b759a7c382303f06429b5ad0664cc3be37bc7c9
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2025-01-31 14:59:45 -0800

    remove evaporating temperature value passing

--- src/EnergyPlus/HVACVariableRefrigerantFlow.hh
+++ src/EnergyPlus/HVACVariableRefrigerantFlow.hh
@@ -448,7 +448,7 @@ namespace HVACVariableRefrigerantFlow {
 
         void SizeVRFCondenser(EnergyPlusData &state);
 
-        void CalcVRFCondenser_FluidTCtrl(EnergyPlusData &state, const bool FirstHVACIteration);
+        void CalcVRFCondenser_FluidTCtrl(EnergyPlusData &state);
 
         void CalcVRFIUTeTc_FluidTCtrl(EnergyPlusData &state);
 

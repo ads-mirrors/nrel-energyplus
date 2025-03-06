221cac47e8dc6c64ecc40612d50be48e23537f53
Edwin Lee <leeed2001@gmail.com>
2025-03-05 21:41:05 -0600

    Trying to fix signage...@jmarrec? [decent_ci_skip]

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -678,6 +678,7 @@ if(APPLE)
       "runenergyplus"
       "runepmacro"
       "runreadvars"
+      "launcher.sh"
       # Copied-verbatim apps: Already signed because just copied from bin to package
       # "EPMacro"
       # "PreProcess/EP-Launch-Lite.app"

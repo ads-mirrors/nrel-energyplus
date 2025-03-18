a0fbf8d39c4b8cceef8489fb4b2717ef3b57c8de
Edwin Lee <leeed2001@gmail.com>
2025-03-18 12:01:39 -0500

    Fixed up the windows_gui_launcher script [decent_ci_skip] [actions skip]

--- src/EnergyPlus/WindowsGuiLauncher.cc
+++ src/EnergyPlus/WindowsGuiLauncher.cc
@@ -1,7 +1,7 @@
 #include <windows.h>
 #include <string>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
     // Get the path of the current executable
     wchar_t path[MAX_PATH];
     if (GetModuleFileNameW(NULL, path, MAX_PATH) == 0) {
@@ -25,19 +25,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 1;
     }
 
+    // Create the command line arguments
+    std::wstring args = L"auxiliary ";
+    args += lpCmdLine;
+
     // Launch the target application
-    HINSTANCE hInstance = ShellExecuteW(
+    HINSTANCE hInstance2 = ShellExecuteW(
         NULL,         // Parent window
         L"open",      // Operation
         targetApp.c_str(), // Executable path
-        L"auxiliary " + lpCmdLine, // Arguments
+        args.c_str(),              // Arguments
         directory.c_str(), // Working directory
         SW_HIDE       // Hide console
     );
 
     // Error handling
-    if ((intptr_t)hInstance <= 32) {
-        std::wstring errorMsg = L"Failed to launch application! Error code: " + std::to_wstring((intptr_t)hInstance);
+    if ((intptr_t)hInstance2 <= 32) {
+        std::wstring errorMsg = L"Failed to launch application! Error code: " + std::to_wstring((intptr_t)hInstance2);
         MessageBoxW(NULL, errorMsg.c_str(), L"Error", MB_ICONERROR | MB_OK);
         return 1;
     }

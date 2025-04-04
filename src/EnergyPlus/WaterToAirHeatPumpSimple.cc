7ac97470a3542f25e8e4510c9566ff18a6985a09
Lerond <jeremy.lerond@pnnl.gov>
2025-04-04 15:02:37 -0700

    Add check for companion coil autosizing.

--- src/EnergyPlus/WaterToAirHeatPumpSimple.cc
+++ src/EnergyPlus/WaterToAirHeatPumpSimple.cc
@@ -2876,18 +2876,21 @@ namespace WaterToAirHeatPumpSimple {
                     if (simpleWatertoAirHP.CompanionCoolingCoilNum > 0) {
                         auto const &companionCoolingCoil =
                             state.dataWaterToAirHeatPumpSimple->SimpleWatertoAirHP(simpleWatertoAirHP.CompanionCoolingCoilNum);
-                        int PltSizNumCompanionCoil = 0;
-                        PltSizNumCompanionCoil = PlantUtilities::MyPlantSizingIndex(
-                            state,
-                            format("COIL:{}:WATERTOAIRHEATPUMP:EQUATIONFIT", WatertoAirHPNamesUC[static_cast<int>(companionCoolingCoil.WAHPType)]),
-                            companionCoolingCoil.Name,
-                            companionCoolingCoil.WaterInletNodeNum,
-                            companionCoolingCoil.WaterOutletNodeNum,
-                            ErrorsFound,
-                            false);
-                        RatedWaterVolFlowRateDes = max(RatedWaterVolFlowRateDes,
-                                                       (1 + 1 / RatedCoolCOP) * companionCoolingCoil.RatedCapCoolTotal /
-                                                           (state.dataSize->PlantSizData(PltSizNumCompanionCoil).DeltaT * Cp * rho));
+                        if (companionCoolingCoil.RatedCapCoolTotal != DataSizing::AutoSize) {
+                            int PltSizNumCompanionCoil = 0;
+                            PltSizNumCompanionCoil =
+                                PlantUtilities::MyPlantSizingIndex(state,
+                                                                   format("COIL:{}:WATERTOAIRHEATPUMP:EQUATIONFIT",
+                                                                          WatertoAirHPNamesUC[static_cast<int>(companionCoolingCoil.WAHPType)]),
+                                                                   companionCoolingCoil.Name,
+                                                                   companionCoolingCoil.WaterInletNodeNum,
+                                                                   companionCoolingCoil.WaterOutletNodeNum,
+                                                                   ErrorsFound,
+                                                                   false);
+                            RatedWaterVolFlowRateDes = max(RatedWaterVolFlowRateDes,
+                                                           (1 + 1 / RatedCoolCOP) * companionCoolingCoil.RatedCapCoolTotal /
+                                                               (state.dataSize->PlantSizData(PltSizNumCompanionCoil).DeltaT * Cp * rho));
+                        }
                     }
                 } else if (simpleWatertoAirHP.WAHPType == WatertoAirHP::Cooling) {
                     //       use companion heating coil capacity to calculate volumetric flow rate

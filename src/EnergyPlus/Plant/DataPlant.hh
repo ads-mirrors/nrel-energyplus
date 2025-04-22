19763f27c672e6a5d704d01e1b6adf04257ac847
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2025-04-22 15:41:09 -0700

    add new AWHP to PlantEquipTypeNames

--- src/EnergyPlus/Plant/DataPlant.hh
+++ src/EnergyPlus/Plant/DataPlant.hh
@@ -182,6 +182,8 @@ namespace DataPlant {
         "HeatPump:PlantLoop:EIR:Heating",
         "HeatPump:AirToWater:FuelFired:Cooling",
         "HeatPump:AirToWater:FuelFired:Heating",
+        "HeatPump:AirToWater:Cooling",
+        "HeatPump:AirToWater:Heating",
         "DistrictHeating:Steam"};
 
     static constexpr std::array<std::string_view, static_cast<size_t>(PlantEquipmentType::Num)> PlantEquipTypeNamesUC{
@@ -284,6 +286,8 @@ namespace DataPlant {
         "HEATPUMP:PLANTLOOP:EIR:HEATING",
         "HEATPUMP:AIRTOWATER:FUELFIRED:COOLING",
         "HEATPUMP:AIRTOWATER:FUELFIRED:HEATING",
+        "HEATPUMP:AIRTOWATER:COOLING",
+        "HEATPUMP:AIRTOWATER:HEATING",
         "DISTRICTHEATING:STEAM"};
 
     static constexpr std::array<LoopType, static_cast<size_t>(PlantEquipmentType::Num)> ValidLoopEquipTypes{

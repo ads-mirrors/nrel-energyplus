171eb366086a66c5dc42ff110bc3a9b376d85744
kevin-moos <kevin.moos@bigladdersoftware.com>
2025-09-29 23:01:35 -0500

    Apply clang format

--- tst/EnergyPlus/unit/BoilerHotWater.unit.cc
+++ tst/EnergyPlus/unit/BoilerHotWater.unit.cc
@@ -220,14 +220,15 @@ TEST_F(EnergyPlusFixture, Boiler_HotWater_ZeroNominalCapacity)
 
     EXPECT_FALSE(process_idf(idf_objects, false));
 
-    std::string const expected_error = delimited_string({
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - \"0\" - Expected number greater than 0.000000",
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against child schema #0.",
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Value type \"number\" for input \"0\" not permitted by 'type' constraint.",
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - \"0\" - Failed to match against any enum values.",
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against child schema #1.",
-        "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against any schemas allowed by anyOf constraint."
-    });
+    std::string const expected_error = delimited_string(
+        {"   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - \"0\" - Expected number greater than 0.000000",
+         "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against child schema #0.",
+         "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Value type \"number\" for input \"0\" not permitted by 'type' "
+         "constraint.",
+         "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - \"0\" - Failed to match against any enum values.",
+         "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against child schema #1.",
+         "   ** Severe  ** <root>[Boiler:HotWater][Central Boiler][nominal_capacity] - Failed to validate against any schemas allowed by anyOf "
+         "constraint."});
     compare_err_stream(expected_error, true);
 }
 
